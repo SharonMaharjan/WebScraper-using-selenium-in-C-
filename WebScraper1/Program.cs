e9421c9348bd265ea6b3d9d7f425761c4cd60ca9
Sharon <r0729187@student.thomasmore.be>
2022-12-13 22:16:52 +0100

    it can see the data

---
+++ WebScraper1/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace WebScraper1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            //var searchInput =  driver.FindElement(By.Name("q"));
+             var searchInput =  driver.FindElement(By.CssSelector(".gLFyf"));
+
+             searchInput.SendKeys("resturant");
+             searchInput.Submit();
+
+             var titles = driver.FindElements(By.XPath("//div[@role='heading']"));
+
+             foreach( var item in titles)
+             {
+                Console.WriteLine(item.Text);
+             }
+
+            //var firstResult = driver.FindElement(By.ClassName("yuRUbf"));
+            //var link = firstResult.FindElement(By.TagName("a"));
+
+            //driver.Navigate().GoToUrl(link.GetAttribute("href"));
+        }
+    }
+}
