5aee1583f2d0de3c546e0d545b5fda15b2088377
Sharon Maharjan <r0729187@student.thomasmore.be>
2022-12-14 20:08:54 +0100

    write in csv & json

---
+++ WebScraper1/JsonFileUtils.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WebScraper1
+{
+    public static class JsonFileUtils
+    {
+        private static readonly JsonSerializerOptions _options =
+            new() { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+
+        public static void SimpleWrite(object obj, string fileName)
+        {
+            var jsonString = System.Text.Json.JsonSerializer.Serialize(obj, _options);
+            File.WriteAllText(fileName, jsonString);
+        }
+    }
+
+    //public static class JsonFileUtils
+    //{
+    //    private static readonly JsonSerializerSettings _options
+    //        = new() { NullValueHandling = NullValueHandling.Ignore };
+
+    //    public static void SimpleWrite(object obj, string fileName)
+    //    {
+    //        var jsonString = JsonConvert.SerializeObject(obj, _options);
+    //        File.WriteAllText(fileName, jsonString);
+    //    }
+    //}
+}
--- WebScraper1/Program.cs
+++ WebScraper1/Program.cs
@@ -1,6 +1,8 @@
-﻿using OpenQA.Selenium;
+﻿using CsvHelper;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Globalization;
 
 namespace WebScraper1
 {
@@ -18,16 +20,35 @@ namespace WebScraper1
              searchInput.Submit();
 
              var titles = driver.FindElements(By.XPath("//div[@role='heading']"));
+            var output = new List<Record>();
 
-             foreach( var item in titles)
+            foreach ( var item in titles)
              {
                 Console.WriteLine(item.Text);
+                output.Add(new Record() { Item= item.Text});
              }
+
 
             //var firstResult = driver.FindElement(By.ClassName("yuRUbf"));
             //var link = firstResult.FindElement(By.TagName("a"));
 
             //driver.Navigate().GoToUrl(link.GetAttribute("href"));
+
+            // reference: https://code-maze.com/csharp-writing-csv-file/
+            using (var writer = new StreamWriter("output.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(output);
+            }
+
+            // reference: https://code-maze.com/csharp-write-json-into-a-file/
+            JsonFileUtils.SimpleWrite(output, "output.json");
         }
     }
+
+    public class Record
+    {
+        public string Item { get; set; }
+    }
+
 }
